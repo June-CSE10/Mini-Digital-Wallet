fa8881e9d96813d6f8dcb0e2509356f00ee2bf37
Zacky <Zacky@DESKTOP-U528AV0>
2026-06-24 10:41:48 +0700

    update register

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Mini_Digital_Wallet.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Cs {
+            get {
+                object obj = ResourceManager.GetObject("Cs", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
